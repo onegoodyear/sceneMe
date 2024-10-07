70591bf63a82e1f6a1b43c2a261ef44596651a65
abdellatif feghouli <wesbedouinson@gmail.com>
2024-10-07 13:40:06 +0200

    Home page adjustments

--- client/src/components/Row.js
+++ client/src/components/Row.js
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import MovieCard from "./MovieCard.js";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
-const Row = ({ title, fetchUrl }) => {
+const Row = ({ title, fetchUrl, rowId }) => {
   const [medias, setMedias] = useState([]);
 
   useEffect(() => {
@@ -11,12 +12,26 @@ const Row = ({ title, fetchUrl }) => {
     });
   }, [fetchUrl]);
 
+  const slideLeft = () => {
+    let slider = document.getElementById("slider" + rowId);
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+  const slideRight = () => {
+    let slider = document.getElementById("slider" + rowId);
+    slider.scrollRight = slider.scrollRight + 500;
+  };
+
   return (
     <div className="container">
       <h1 className="text-white font-bold my-4">{title}</h1>
-      <div className="relative flex items-center">
+      <div className="relative flex items-center group">
+        <MdChevronLeft
+          size={40}
+          className="bg-white rounded-full absolute left-0 opacity-50 hover:opacity-100 cursor-pointer hidden group-hover:block z-10"
+          onClick={slideLeft}
+        />
         <div
-          id={"slider"}
+          id={"slider" + rowId}
           className="whitespace-nowrap scroll-smooth overflow-x-scroll no-scrollbar"
         >
           {medias?.map((item, id) => (
@@ -26,6 +41,11 @@ const Row = ({ title, fetchUrl }) => {
             />
           ))}
         </div>
+        <MdChevronRight
+          size={40}
+          className="bg-white rounded-full absolute right-0 opacity-50 hover:opacity-100 cursor-pointer hidden group-hover:block z-10"
+          onClick={slideRight}
+        />
       </div>
     </div>
   );
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,18 +7,22 @@ const Home = () => {
     <>
       <Main />
       <Row
+        rowId={1}
         title="Top Rated Movies"
         fetchUrl={requests.topRatedMovies}
       />
       <Row
+        rowId={2}
         title="Top Rated Shows"
         fetchUrl={requests.topRatedShows}
       />
       <Row
+        rowId={3}
         title="Trending"
         fetchUrl={requests.trending}
       />
       <Row
+        rowId={4}
         title="Up Coming"
         fetchUrl={requests.upComing}
       />
