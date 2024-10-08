e2f71ca6fd8fcf73a6f05f54de6d421069776956
onegoodyear <wesbedouinson@gmail.com>
2024-10-08 17:03:01 +0200

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -31,7 +31,7 @@ exports.loginUser = async (req, res) => {
     const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
       expiresIn: "1h",
     });
-    onsole.log(token);
+    console.log(token);
     return res.status(200).send({ message: "Login successful", user, token });
   } catch (err) {
     res.status(400).send(err);
